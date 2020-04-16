e4bf282b52f7aafd4a2980da7db1c7191b383d5b
Edo Novanto <novantoedo@gmail.com>
2020-04-16 19:42:49 +0700

    initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use  App\User;
+
+class UserController extends Controller
+{
+     /**
+     * Instantiate a new UserController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Get the authenticated User.
+     *
+     * @return Response
+     */
+    public function profile()
+    {
+        return response()->json(['user' => Auth::user()], 200);
+    }
+
+    /**
+     * Get all User.
+     *
+     * @return Response
+     */
+    public function allUsers()
+    {
+         return response()->json(['users' =>  User::all()], 200);
+    }
+
+    /**
+     * Get one user.
+     *
+     * @return Response
+     */
+    public function singleUser($id)
+    {
+        try {
+            $user = User::findOrFail($id);
+
+            return response()->json(['user' => $user], 200);
+
+        } catch (\Exception $e) {
+
+            return response()->json(['message' => 'user not found!'], 404);
+        }
+
+    }
+
+    public function update(Request $request, $id){
+        $result = User::find($id);
+        $result->fill($request->all());
+        $result->save();
+
+        if($result){
+            $data['code'] = 200;
+            $data['result'] = $result;
+        }
+        else {
+            $data['code'] = 500;
+            $data['result'] = 'error';
+        }
+        
+        return response($data);
+    }
+
+}
